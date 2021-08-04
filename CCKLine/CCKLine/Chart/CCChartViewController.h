1096db5b9af764dfcf5de0e9f3788847e4d3bf29
tsaievan <tsaievan@icloud.com>
2021-08-04 14:56:44 +0800

    创建CCChartViewController

---
+++ CCKLine/CCKLine/Chart/CCChartViewController.h
@@ -0,0 +1,16 @@
+//
+//  CCChartViewController.h
+//  CCKLine
+//
+//  Created by 蔡一凡 on 2021/8/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCChartViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCKLine/CCKLine/Chart/CCChartViewController.m
@@ -0,0 +1,31 @@
+//
+//  CCChartViewController.m
+//  CCKLine
+//
+//  Created by 蔡一凡 on 2021/8/4.
+//
+
+#import "CCChartViewController.h"
+
+@interface CCChartViewController ()
+
+@end
+
+@implementation CCChartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
