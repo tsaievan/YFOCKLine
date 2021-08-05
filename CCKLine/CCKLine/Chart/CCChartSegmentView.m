2cdea20a24f75139260de9c2b41dce84515774a3
tsaievan <tsaievan@icloud.com>
2021-08-05 12:00:07 +0800

    开始创建segmentView的一些控件

--- CCKLine/CCKLine/Chart/CCChartSegmentView.m
+++ CCKLine/CCKLine/Chart/CCChartSegmentView.m
@@ -6,15 +6,32 @@
 //
 
 #import "CCChartSegmentView.h"
+#import "UIColor+CCKLine.h"
+
+static NSInteger const CCChartSegmentStartTag = 2000;
+@interface CCChartSegmentView ()
+@property (nonatomic, strong) UIButton *selectedButton;
+@property (nonatomic, strong) UIView *indicatorView;
+@property (nonatomic, strong) UIButton *secondLevelSelectedButton1;
+@property (nonatomic, strong) UIButton *secondLevelSelectedButton2;
+@end
 
 @implementation CCChartSegmentView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithItems:(NSArray *)items {
+    if (self = [super initWithFrame:CGRectZero]) {
+        self.items = items;
+    }
+    return self;
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.clipsToBounds = YES;
+        self.backgroundColor = UIColor.assistBackgroundColor;
+    }
+    return self;
+}
+
 
 @end
