f8df0f691e27dca29a8e94f0f6f97044f9fc782e
tsaievan <tsaievan@icloud.com>
2021-08-05 16:49:06 +0800

    调整一下文件结构

--- CCKLine/CCKLine/Chart/CCChartView.m
+++ CCKLine/CCKLine/Chart/CCChartView.m
@@ -6,11 +6,36 @@
 //
 
 #import "CCChartView.h"
+#import "UIColor+CCKLine.h"
+#import <Masonry/Masonry.h>
+
+@interface CCChartView () <CCChartSegmentViewDelegate>
+
+@end
 
 @implementation CCChartView
+
+- (instancetype)initWithItemModels:(NSArray *)itemModels {
+    if (self = [super init]) {
+        self.backgroundColor = UIColor.backgroundColor;
+    }
+    return self;
+}
+
+- (void)initUI {
+    self.segmentView = [[CCChartSegmentView alloc] init];
+    self.segmentView.delegate = self;
+    [self addSubview:_segmentView];
+    [self.segmentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.left.top.mas_equalTo(self);
+        make.width.mas_equalTo(50);
+    }];
+}
 
 @end
 
 @implementation CCChartViewItemModel
+
+
 
 @end
