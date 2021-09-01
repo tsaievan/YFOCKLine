2aa1cb6168661cf36c290a5b785fa7ab3df09696
tsaievan <tsaievan@icloud.com>
2021-09-01 17:05:05 +0800

    新增CCEMAPainter

---
+++ CCKLine/CCKLine/KLine/Painter/CCEMA/CCEMAPainter.h
@@ -0,0 +1,17 @@
+//
+//  CCEMAPainter.h
+//  CCKLine
+//
+//  Created by 蔡一凡 on 2021/9/1.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "CCPainterProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCEMAPainter : CALayer <CCPainterProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
