8d848b2be7c26a5a3e50218d9656530eab92d8e6
tsaievan <tsaievan@icloud.com>
2021-09-01 15:10:19 +0800

    添加CCRSIPainter

---
+++ CCKLine/CCKLine/KLine/Painter/CCRSI/CCRSIPainter.h
@@ -0,0 +1,17 @@
+//
+//  CCRSIPainter.h
+//  CCKLine
+//
+//  Created by 蔡一凡 on 2021/9/1.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "CCPainterProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCRSIPainter : CALayer <CCPainterProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
